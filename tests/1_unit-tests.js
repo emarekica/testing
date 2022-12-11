1e18bb14fa1cf461efdc41d2444a1bb14b3c4619
emarekica <emarekica@gmail.com>
2022-12-11 19:43:40 +0100

    add new unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -134,9 +134,12 @@ suite("Arrays", function () {
   });
 
   // #12
+
+  /// The assertions are checking against variables defined before the ‘Arrays’ suite of tests, check carefully whether the array includes the value being asserted.
+  // assert.include() and assert.notInclude() parameters take the form (haystack, needle, message) where the needle is what you are searching for in the haystack. The message provides feedback where there is an error.
   test("Array #include, #notInclude", function () {
-    assert.fail(winterMonths, "jul", "It's summer in july...");
-    assert.fail(backendLanguages, "javascript", "JS is a backend language");
+    assert.notInclude(winterMonths, "jul", "It's summer in july...");
+    assert.include(backendLanguages, "javascript", "JS is a backend language");
   });
 });
 
@@ -148,20 +151,28 @@ const formatPeople = function (name, age) {
 suite("Strings", function () {
   // #13
   test("#isString, #isNotString", function () {
-    assert.fail(Math.sin(Math.PI / 4), "A float is not a string");
-    assert.fail(process.env.PATH, "An env variable is a string (or undefined)");
-    assert.fail(JSON.stringify({ type: "object" }), "JSON is a string");
+    assert.isNotString(Math.sin(Math.PI / 4), "A float is not a string");
+    assert.isString(
+      process.env.PATH,
+      "An env variable is a string (or undefined)"
+    );
+    assert.isString(JSON.stringify({ type: "object" }), "JSON is a string");
   });
+
   // #14
+
+  // assert.include() and assert.notInclude() parameters take the form (haystack, needle, message) where the needle is what you are searching for in the haystack. The message provides feedback where there is an error.
+
   test("String #include, #notInclude", function () {
-    assert.fail("Arrow", "row", "'Arrow' contains 'row'");
-    assert.fail("dart", "queue", "But 'dart' doesn't contain 'queue'");
+    assert.include("Arrow", "row", "'Arrow' contains 'row'");
+    assert.notInclude("dart", "queue", "But 'dart' doesn't contain 'queue'");
   });
+
   // #15
   test("#match, #notMatch", function () {
     const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/;
-    assert.fail(formatPeople("John Doe", 35), regex);
-    assert.fail(formatPeople("Paul Smith III", "twenty-four"), regex);
+    assert.match(formatPeople("John Doe", 35), regex);
+    assert.notMatch(formatPeople("Paul Smith III", "twenty-four"), regex);
   });
 });
 
